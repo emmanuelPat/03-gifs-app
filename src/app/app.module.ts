b8c3711426c6514dd342c7ca48d0eb9055f0ee76
Emmanuel Patiño <emmanuel.patino@ipiam.edu.mx>
2023-09-06 17:00:54 -0600

    Fin sección Loader

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { GifsModule } from './gifs/gifs.module';
+import { SharedModule } from './shared/shared.module';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    GifsModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -0,0 +1,34 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
+
+@Component({
+  selector: 'gifs-search-box',
+  template: `
+    <h5>Buscar:</h5>
+    <input type="text"
+      class="form-control"
+      placeholder="Buscar gifs"
+      (keyup.enter)="searchTag()"
+      #txtTagInput
+    >
+  `
+})
+
+export class SearchBoxComponent {
+  constructor( private gifsService: GifsService ) { }
+
+  @ViewChild('txtTagInput')
+  public tagInput!: ElementRef<HTMLInputElement>
+
+  searchTag(): void {
+    const newTag = this.tagInput.nativeElement.value;
+
+    this.gifsService.searchTag(newTag);
+    this.tagInput.nativeElement.value = '';
+    // console.log( {newTag} );
+  }
+
+  // searchTag( newTag: string ): void {
+  //   console.log( {newTag} );
+  // }
+}
